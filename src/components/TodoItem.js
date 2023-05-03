f66030244e6ac1f20354a8ec9cfde00d6708d6aa
ClintonYounge <clintonyounge18@gmail.com>
2023-05-03 00:31:27 -0400

    fix linter error

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import styles from '../styles/TodoItem.module.css';
 import PropTypes from 'prop-types';
+import styles from '../styles/TodoItem.module.css';
 
 const TodoItem = ({
   itemProp, handleChange, delTodo, setUpdate,
