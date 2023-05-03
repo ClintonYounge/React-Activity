33b99f2398d6534768a9ff8b9a4b6466e8dcc0d9
ClintonYounge <clintonyounge18@gmail.com>
2023-05-03 00:28:01 -0400

    re-run npm init

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from '../styles/TodoItem.module.css';
+import PropTypes from 'prop-types';
 
 const TodoItem = ({
   itemProp, handleChange, delTodo, setUpdate,
